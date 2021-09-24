c9ef845ced033ec8c5099868535fc34cd38f9fe9
Safa Nasirli <admin@admins-air.myfiosgateway.com>
2021-09-24 10:04:55 -0400

    Hello World

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
 
-(req, res) => res.send('Hello World!')
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 app.listen(4000, () => console.log('Server is running'))     
