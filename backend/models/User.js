9b1267123efb3a0ea1bfb0202e2e331ff89ce679
Christina Whethers <flandresamarawr@gmail.com>
2021-09-24 15:17:26 -0400

    Made user and pokemon models

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("../database/connection")
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model("User", userSchema)
