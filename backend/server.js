b6065306f04aaadf6dd5afc880b6f357dd455354
Annie Lee <annie80113@hotmail.com>
2021-09-24 15:41:02 -0400

    organized code

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 4000;
-const cors = require('cors');
 
 // Importing Schema
 
