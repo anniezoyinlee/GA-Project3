0f72649e43b836132e18622d99427646fa4a76b8
Christina Whethers <flandresamarawr@gmail.com>
2021-09-26 15:51:35 -0400

    pokemon data auto seeds on front page, plus hello world placeholder

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
+const Pokemon = require("./models/Pokemon")
+const seedData = require("./database/seeds.json")
 
 // controllers
 const pokemonController = require('./controllers/pokemons');
@@ -22,6 +24,10 @@ app.use(cors())
 // Sample homepage
 
 app.get("/", (req, res, next) => {
+    Pokemon.deleteMany()
+        .then(() => Pokemon.insertMany(seedData))
+        .then(console.log)
+        .catch(console.error)
     res.send("Hello World!")
 })
 
