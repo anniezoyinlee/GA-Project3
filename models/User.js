1afaf269f4089d274e3161a5095c95c74bbb7029
Annie Lee <annie80113@hotmail.com>
2021-09-26 16:22:51 -0400

    removed duplicate folders

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require("../database/connection")
+const mongoose = require("../database/connection");
 
 const userSchema = new mongoose.Schema(
     {
@@ -21,4 +21,4 @@ const userSchema = new mongoose.Schema(
     }
 )
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema);
