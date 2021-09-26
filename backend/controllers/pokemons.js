2e16a0932929bdfb0ec6f103daf66d2ca6ad87f6
Annie Lee <annie80113@hotmail.com>
2021-09-26 12:05:13 -0400

    Create Routes for Pokemon, configure in server

---
+++ backend/controllers/pokemons.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const Pokemon = require('../models/Pokemon');
+const router = express.Router();
+
+// INDEX
+router.get('/', (req, res) => {
+  Pokemon.find().then((pokemons) => res.json(pokemons));
+});
+
+// SHOW
+router.get('/:id', handleValidateId, (req, res) => {
+  Pokemon.findById(req.params.id).then((pokemon) => res.json(pokemon));
+});
+
+// CREATE
+router.post('/', requireToken, (req, res) => {
+  Pokemon.create(req.body).then((pokemon) => res.json(pokemon));
+});
+
+// UPDATE
+router.put('/:id', (req, res) => {
+  Pokemon.findOneAndUpdate(
+    { _id: req.params.id }, 
+    req.body, 
+    {new: true,})
+      .then((pokemon) => res.json(pokemon));
+});
+
+// DESTROY
+// DELETE api/pokemons/5a7db6c74d55bc51bdf39793
+router.delete('/:id', (req, res) => {
+  Pokemon.findOneAndDelete({_id: req.params.id,})
+      .then((pokemon) => res.json(pokemon));
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 
+// controllers
+const pokemonController = require('./controllers/pokemons');
+
 // Importing Schema
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+// Configure the route middleware
+app.use('/api/pokemons', pokemonController);
 
 // cors
 app.use(cors())
@@ -15,7 +21,6 @@ app.use(cors())
 
 // seed route
 
-// controllers
 
 
 app.set("port", process.env.PORT || 4000)
