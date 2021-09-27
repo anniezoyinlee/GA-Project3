8de700d51d2c097b5a148fa53b863c5280891d15
Christina Whethers <flandresamarawr@gmail.com>
2021-09-27 12:23:43 -0400

    User password not returned on signup request

--- models/User.js
+++ models/User.js
@@ -1,24 +1,28 @@
 const mongoose = require("../database/connection");
 
-const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        password: {
-            type: String,
-            required: true
-        }
+const userSchema = new mongoose.Schema({
+    // name: {
+    //     type: String,
+    //     required: true
+    // },
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    {
-        timestamps: true
+    password: {
+        type: String,
+        required: true
     }
-)
+}, {
+    timestamps: true,
+    toJSON: {
+        virtuals: true,
+        transform: (_doc, ret) => {
+            delete ret.password
+            return ret
+        }
+    }
+})
 
 module.exports = mongoose.model("User", userSchema);
